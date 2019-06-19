f3d359e3e3ecb51f53de5d573b3018d39122e6df
Freeman Fang <freeman.fang@gmail.com>
2019-06-19 14:57:47 -0400

    Import xml resource

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
@@ -25,8 +25,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath:endpoint.xml"})
 public class SampleRestApplication {
 
     private Bus bus;
